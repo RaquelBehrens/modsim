9d537383f478326067f742c65e6d2f1aaa0f727c
arthur-plautz <atr.plautz@gmail.com>
2024-05-19 17:08:11 -0300

    Add alternative version and current repository values

--- docker/config.sh
+++ docker/config.sh
@@ -1,8 +1,8 @@
 # Entradas: Digite aqui as suas configurações
 GIT_USERNAME=exemplo
 GIT_USER_EMAIL=exemplo@mail.com
-GIT_REPO=https://github.com/rlcancian2/Genesys-Simulator
-GIT_BRANCH=2023
+GIT_REPO=https://github.com/RaquelBehrens/modsim
+GIT_BRANCH=main
 
 # Saídas: Configurações geradas pela aplicação
 # SSH_PUBLIC_KEY=
