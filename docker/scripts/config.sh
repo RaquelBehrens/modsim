fe468ece398856624db1db73a63b3f57b2e80968
arthur-plautz <atr.plautz@gmail.com>
2024-06-18 15:53:15 -0300

    Add support to executable variables

--- docker/scripts/config.sh
+++ docker/scripts/config.sh
@@ -1,18 +1,26 @@
 # Nome do usuário
 USER=$(whoami)
+
+# Usuário da imagem
 GENESYS_USER="genesys-user"
-GENESYS_ROOT=/home/genesys
-PROJECT_ROOT=$(cd .. && pwd)
+
+# Nome da imagem Docker
+IMAGE_NAME="genesys-image"
 
 # Utilizar cache no build da imagem (1 - sim, 0 - não)
 DOCKER_CACHE=0
 
 # Nome do Dockerfile
 DOCKERFILE_NAME="Dockerfile"
+
+# Driver gráfico
 MESA_LOADER_DRIVER_OVERRIDE=zink
 
-# Nome da imagem Docker
-IMAGE_NAME="genesys-image"
+# Variáveis internas da imagem
+GENESYS_ROOT=/home/genesys
+PROJECT_ROOT=$(cd .. && pwd)
+GENESYS_NOGUI_SUBPATH=GenesysShell
+GENESYS_GUI_SUBPATH=GenesysQtGUI
 
 if ! command -v docker &> /dev/null; then
     echo "Docker não instalado"
--- docker/scripts/shell.sh
+++ docker/scripts/shell.sh
@@ -4,6 +4,7 @@ if docker image inspect $IMAGE_NAME >/dev/null 2>&1; then
     docker run --name genesys --rm -ti --net=host --ipc=host \
         -e DISPLAY=$DISPLAY \
         -e MESA_LOADER_DRIVER_OVERRIDE=$MESA_LOADER_DRIVER_OVERRIDE \
+        -e GENESYS_NOGUI_SUBPATH=$GENESYS_NOGUI_SUBPATH \
         -e QT_X11_NO_MITSHM=1 \
         -e XDG_RUNTIME_DIR=/run/user/1001 \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
