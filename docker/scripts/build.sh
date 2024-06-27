26b4176eb470bb1b63bc46f1acd46a6233931249
arthur-plautz <atr.plautz@gmail.com>
2024-06-27 18:43:50 -0300

    Add docker to integration

---
+++ docker/scripts/build.sh
@@ -0,0 +1,7 @@
+source ./scripts/config.sh
+
+if [ $DOCKER_CACHE == 0 ]; then
+    docker build -f $GENESYS_DOCKERFILE -t $GENESYS_IMAGE --no-cache .
+else
+    docker build -f $GENESYS_DOCKERFILE -t $GENESYS_IMAGE .
+fi
